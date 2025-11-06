1eeb40929a1fa83d97f272f64a1e64a81fe5dade
Ese Agboh <eseagboh@Eses-MacBook-Air.local>
2025-11-06 07:55:22 -0700

    Fixes made to backend files

--- mindstrong_backend/users/urls.py
+++ mindstrong_backend/users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('me/', UserDetail.as_view(), name='user-detail'), 
+    path('me/', UserDetail.as_view(), name='user-detail') 
 ]
 
